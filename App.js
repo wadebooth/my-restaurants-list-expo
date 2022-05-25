a5e9071bc2789c0f650f1cca78e90fc4ad58da45
wadebooth <wademgmt@gmail.com>
2022-05-25 15:50:26 -0400

    added background and restaurant api

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+} from "react-native";
+import { useEffect, useState } from "react";
+
+const image = {
+  uri: "https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg",
+};
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://my-first-firestore-wb.web.app/restaurants"
+        );
+        const data = await response.json();
+        setAllRestaurants(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        resizeMode="cover"
+        source={image}
+        style={styles.container}
+      >
+        {allRestaurants ? (
+          allRestaurants?.map((singleRest) => (
+            <Text styles={styles} key={singleRest.id}>
+              {singleRest.name}
+            </Text>
+          ))
+        ) : (
+          <ActivityIndicator size="large" color="orange" />
+        )}
+        <Text>Drippy</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -16,5 +59,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
   },
 });
